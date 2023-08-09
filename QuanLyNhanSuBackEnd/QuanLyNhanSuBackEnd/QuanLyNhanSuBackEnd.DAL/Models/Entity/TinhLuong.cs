49e8d7936a40cd392f3eb005a7b68e5619aea178
unknown <ble07983@gmail.com>
2023-08-09 17:02:21 +0700

    Add Mode / Entity

---
+++ QuanLyNhanSuBackEnd/QuanLyNhanSuBackEnd/QuanLyNhanSuBackEnd.DAL/Models/Entity/TinhLuong.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyNhanSuBackEnd.DAL.Models.Entity
+{
+    internal class TinhLuong
+    {
+    }
+}
