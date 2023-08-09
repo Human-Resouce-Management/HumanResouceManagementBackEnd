6a8d5954cdbacc3937c849a0d0d45215119ce736
unknown <ble07983@gmail.com>
2023-08-09 18:51:23 +0700

    Up Model/BoPhan

--- QuanLyNhanSuBackEnd/QuanLyNhanSuBackEnd/QuanLyNhanSuBackEnd.DAL/Models/Entity/BoPhan.cs
+++ QuanLyNhanSuBackEnd/QuanLyNhanSuBackEnd/QuanLyNhanSuBackEnd.DAL/Models/Entity/BoPhan.cs
@@ -1,6 +1,7 @@
 ﻿using MayNghien.Common.Models.Entity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,10 @@ namespace QuanLyNhanSuBackEnd.DAL.Models.Entity
     {
         public string? TenBoPhan { get; set; }
         public string? QuanLy { get; set; }
-        public Guid? BoPhanChuQuanId { get; set; }
 
+        [ForeignKey("BoPhan")]
+        public Guid? BoPhanChuQuanId { get; set; }
+        [ForeignKey("BoPhanChuQuanId")]
+        public BoPhan? BoPhanChuQuan { get;set; } 
     }
 }
